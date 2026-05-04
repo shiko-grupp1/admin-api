88ec21854d69d0787ae4ef9901611f720b5d9180
AlayaStHill <alaya.st.hill@gmail.com>
2026-05-04 13:34:53 +0200

    feat: add domain project

---
+++ src/AdminService.Domain/Entities/Students/StudentId.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminService.Domain.Entities.Students;
+
+public sealed record StudentId(Guid Value)
+{
+    public static StudentId Create()
+    {
+        return new StudentId(Guid.NewGuid());
+    }
+}
